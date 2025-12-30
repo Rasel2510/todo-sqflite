b0b7f9b3a8c7f2940220c41e0c56f522c47fedec
rasel2510 <rasel251062@gmail.com>
2025-12-30 10:21:21 +0600

    update

--- lib/features/data/provider.dart
+++ lib/features/data/provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todo/model/model_class.dart';
-import 'package:todo/servise/db_helper.dart';
+import 'package:todo/features/model/add_todo_model.dart';
+import 'package:todo/features/data/db_helper.dart';
 
 class TodoProvider with ChangeNotifier {
   final DbHelper _dbHelper = DbHelper();
@@ -11,7 +11,7 @@ class TodoProvider with ChangeNotifier {
   bool _isLoading = false;
   bool get isLoading => _isLoading;
 
-  // Load all todos
+  // Load all todos from database
   Future<void> loadTodos() async {
     _isLoading = true;
     notifyListeners();
@@ -19,22 +19,30 @@ class TodoProvider with ChangeNotifier {
     try {
       _todos = await _dbHelper.readData();
     } catch (e) {
-      print('Error loading todos: $e');
+      debugPrint('Error loading todos: $e');
     } finally {
       _isLoading = false;
       notifyListeners();
     }
   }
 
-  // Add new todo
+  // Add a new todo
   Future<void> addTodo(ModelClass todo) async {
-    await _dbHelper.insertData(todo);
-    await loadTodos(); 
+    try {
+      await _dbHelper.insertData(todo);
+      await loadTodos();
+    } catch (e) {
+      debugPrint('Error adding todo: $e');
+    }
   }
 
-  // Delete todo
+  // Delete a todo by id
   Future<void> deleteTodo(int id) async {
-    await _dbHelper.deleteData(id);
-    await loadTodos(); 
+    try {
+      await _dbHelper.deleteData(id);
+      await loadTodos();
+    } catch (e) {
+      debugPrint('Error deleting todo: $e');
+    }
   }
 }
